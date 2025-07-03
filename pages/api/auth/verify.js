3f362c7c5c918c4f2541d6a9bd7b32cf83edfa1b
FuNk-y0u <robesckeydangol2006@gmail.com>
2025-07-03 21:22:19 +0545

    Debugging admin bad gateway issue

--- pages/api/auth/verify.js
+++ pages/api/auth/verify.js
@@ -15,30 +15,37 @@ export default async function verify(req, res){
 
 
         try{
-        var admin = await prisma.Admin.findUnique({
-            where: {
-                id: 1
-            }
-        });
-
-        var pass_cmp = await bcrypt_compare(password, admin.passwordHash);
-
-        if (!pass_cmp){
-            res.status(200).json({
-                message: "Invalid password",
-                success: false
+            console.log("Debug 1");
+            var admin = await prisma.Admin.findUnique({
+                where: {
+                    id: 1
+                }
             });
-            return;
-        }
+            console.log("Debug 2");
 
-        let token = gen_admin_token();
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader("Set-Cookie", `auth=${token}`);
-        res.status(200).json({
-            token: token,
-            message: "Login sucessfull.",
-            success: true
-        });
+            var pass_cmp = await bcrypt_compare(password, admin.passwordHash);
+
+            console.log("Debug 3");
+
+            if (!pass_cmp){
+                res.status(200).json({
+                    message: "Invalid password",
+                    success: false
+                });
+                return;
+            }
+
+            console.log("Debug 4");
+
+            let token = gen_admin_token();
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader("Set-Cookie", `auth=${token}`);
+            res.status(200).json({
+                token: token,
+                message: "Login sucessfull.",
+                success: true
+            });
+            console.log("Debug 5");
         }
         catch(error){
 
